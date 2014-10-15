e766c6679bd260797f5516acffbf62bad910ed2b
GuanglinDu <guanglin.du@gmail.com>
2014-10-15 15:14:49 +0800

    Create a HelloGithub class by DGL

--- hell_github.py
+++ hell_github.py
@@ -1,1 +1,25 @@
-print("hello, github")
+"""
+The 1st test project on github to learn how to collaborate, created by Jia Lia & Guanglin Du.
+Oct. 15, 2014 Wed.
+"""
+
+class HelloGit(object):
+	
+	def __init__(self):
+		self.name = "github"
+
+		
+	def hello_github(self):
+		print("hello, github. From goodmaoxixi on Oct. 15. 2014 Wed.")
+
+	
+	def say_something(self, name = "github"):
+		self.name = name
+		print("Hello, %s!" % self.name)
+
+
+if __name__ == '__main__':
+	hg = HelloGit()
+	hg.hello_github()
+	hg.say_something()
+	
