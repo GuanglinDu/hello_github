0e65e7d180dc727507057b77e1dc5757c2f3b160
Guanglin Du <guanglin.du@gmail.com>
2017-12-11 09:40:25 +0800

    Modify tests/unit_test_demo.py

--- tests/unit_test_demo.py
+++ tests/unit_test_demo.py
@@ -1,5 +1,14 @@
 # https://docs.python.org/3/library/unittest.html
-# Run: python -m unittest unit_test_demo
+# python -m unit_test -h
+# Run:
+# 1. python unit_test_demo.py
+# 2. python unit_test_demo.py -v
+# 3. python -m unittest unit_test_demo
+# 3. python -m unittest  -v unit_test_demo
+
+# Test Discovery:
+# python -m unittest discover -s project_directory -p "test_*.py" -v
+# python -m unittest discover project_directory "test_*.py" -v
 
 import unittest
 
@@ -11,7 +20,7 @@ class MyTest(unittest.TestCase):
         self.assertEqual(fun(3), 4)
 
     def test_should_not_equal(self):
-        self.assertEqual(fun(4), 10)
+        self.assertNotEqual(fun(4), 10)
 
 if __name__ == "__main__":
     unittest.main()
